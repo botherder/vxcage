45fde6d4303e79d42851d0c3baf298fa89ce7127
jekil <alessandro@tanasi.it>
2012-11-06 20:19:42 +0100

    Nested folders

--- utils.py
+++ utils.py
@@ -33,7 +33,7 @@ def jsonize(data):
 def store_sample(data):
     md5 = File(file_data=data).get_sha256()
     
-    folder = os.path.join(Config().api.repository, sha256[0:3])
+    folder = os.path.join(Config().api.repository, sha256[0], sha256[1], sha256[2], sha256[3])
     if not os.path.exists(folder):
         os.makedirs(folder, 0750)
 
@@ -47,7 +47,7 @@ def store_sample(data):
     return file_path
 
 def get_sample_path(sha256):
-    path = os.path.join(Config().api.repository, sha256[0:3], sha256)
+    path = os.path.join(Config().api.repository, sha256[0], sha256[1], sha256[2], sha256[3], sha256)
     if not os.path.exists(path):
         return None
     return path
